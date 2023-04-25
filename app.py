ce36f3035a978995728a2fe08d35b3edd8f17496
jrc <mail>
2023-04-25 18:04:34 +0200

    Added default endpoint and styles

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask
-from flask_restful import Api
+from flask import Flask, Response, render_template
+from flask_restful import Api, Resource
 from resources.Decks import MetaDecks
 from resources.Events import LastEvents
 
@@ -10,8 +10,13 @@ api = Api(app)
 
 #Add resources 
 
+class Default(Resource):
+    def get(self):
+        return Response(render_template('index.html'), mimetype='text/html')
+
 api.add_resource(MetaDecks,'/metadecks/<string:meta>')
 api.add_resource(LastEvents,'/last-events')
+api.add_resource(Default,'/')
 
 if __name__ == '__main__':
     app.run()
