bda81784527eccb671034e2e65e9e9e54c23d6ab
jrc <mail>
2023-04-25 21:52:55 +0200

    Added default error handler

--- app.py
+++ app.py
@@ -3,20 +3,28 @@ from flask_restful import Api, Resource
 from resources.Decks import MetaDecks
 from resources.Events import LastEvents
 
+from webargs import fields, validate
+from webargs.flaskparser import use_args, use_kwargs, parser, abort
+
 #App instance
 
 app= Flask(__name__)
 api = Api(app)
 
-#Add resources 
-
 class Default(Resource):
     def get(self):
         return Response(render_template('index.html'), mimetype='text/html')
 
-api.add_resource(MetaDecks,'/metadecks/<string:meta>')
+#Add resources 
+api.add_resource(MetaDecks,'/metadecks')
 api.add_resource(LastEvents,'/last-events')
 api.add_resource(Default,'/')
+
+#Handle errors
+@parser.error_handler
+def handle_request_parsing_error(err, req, schema, *, error_status_code, error_headers):
+    
+    abort(400, error=str(err))
 
 if __name__ == '__main__':
     app.run()
--- resources/Decks.py
+++ resources/Decks.py
@@ -1,6 +1,13 @@
 from logic.DecksLogic import getDecksByMeta
 from flask_restful import Resource
+from webargs import fields, validate
+from webargs.flaskparser import use_args, parser, abort
 
 class MetaDecks(Resource):
-    def get(self,meta):
-        return getDecksByMeta(meta)
+    args_meta = {'meta':fields.Str(validate=validate.OneOf(['standard','modern','pioneer']))}
+    @use_args(args_meta, location='query') #querystring
+    def get(self, args):
+        return getDecksByMeta(args['meta'])
+
+    
+   
